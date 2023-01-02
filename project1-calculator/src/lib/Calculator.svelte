60d5f27a3fb86508fc8d6b943b4ac8360ce95201
Paul Adams <ghusiyy@gmail.com>
2023-01-02 00:01:23 -0800

    UI enhancement

--- project1-calculator/src/lib/Calculator.svelte
+++ project1-calculator/src/lib/Calculator.svelte
@@ -18,22 +18,28 @@
   .Calculator {
     display: flex;
     flex-direction: column;
+    justify-content: space-evenly;
     height: 500px;
     width: 400px;
     padding: 20px;
+    background-color: #36454f;
   }
   .Input-container {
-    height: 50px;
-    margin-bottom: 20px;
+    height: 80px;
     text-align: right;
+    background-color: white;
   }
   .Input-screen {
+    height: 100%;
+    display: flex;
+    justify-content: end;
+    align-items: center;
     margin-right: 10px;
     font-size: 22px;
   }
   .Button-container {
     display: grid;
     grid-template-columns: repeat(4, 1fr);
-    gap: 10px;
+    gap: 18px;
   }
 </style>
