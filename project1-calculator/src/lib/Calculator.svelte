1e9a5459e481614eb005476fc52e96d246197cf5
Paul Adams <ghusiyy@gmail.com>
2023-01-01 23:46:14 -0800

    build calculator UI

---
+++ project1-calculator/src/lib/Calculator.svelte
@@ -0,0 +1,39 @@
+<script lang="ts">
+  import { keys } from "./CalculatorKeys";
+  let screenInput = "0";
+</script>
+
+<div class="Calculator">
+  <div class="Input-container">
+    <div class="Input-screen">{screenInput}</div>
+  </div>
+  <div class="Button-container">
+    {#each keys as key}
+      <button>{key.label}</button>
+    {/each}
+  </div>
+</div>
+
+<style>
+  .Calculator {
+    display: flex;
+    flex-direction: column;
+    height: 500px;
+    width: 400px;
+    padding: 20px;
+  }
+  .Input-container {
+    height: 50px;
+    margin-bottom: 20px;
+    text-align: right;
+  }
+  .Input-screen {
+    margin-right: 10px;
+    font-size: 22px;
+  }
+  .Button-container {
+    display: grid;
+    grid-template-columns: repeat(4, 1fr);
+    gap: 10px;
+  }
+</style>
---
+++ project1-calculator/src/lib/CalculatorKeys.ts
@@ -0,0 +1,18 @@
+export const keys: Array<{ label: string; value: string | number }> = [
+  { label: "7", value: 7 },
+  { label: "8", value: 8 },
+  { label: "9", value: 9 },
+  { label: "÷", value: "/" },
+  { label: "4", value: 4 },
+  { label: "5", value: 5 },
+  { label: "6", value: 6 },
+  { label: "×", value: "*" },
+  { label: "1", value: 1 },
+  { label: "2", value: 2 },
+  { label: "3", value: 3 },
+  { label: "+", value: "+" },
+  { label: "0", value: 0 },
+  { label: ".", value: "." },
+  { label: "=", value: "=" },
+  { label: "-", value: "-" },
+];
