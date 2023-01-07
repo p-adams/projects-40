dae2e66256b716ff87560b73f0a62e3df0655a95
Paul Adams <ghusiyy@gmail.com>
2023-01-07 00:48:54 -0800

    display quiz question and choices

---
+++ project2-quiz/src/data/questions.ts
@@ -0,0 +1,15 @@
+export const quizQuestions = [
+  {
+    id: 0,
+    question: "Which of the following is not a position value",
+    answer: "D",
+    hint: null,
+    selected: "A",
+    choices: [
+      { label: "fixed", value: "A" },
+      { label: "relative", value: "B" },
+      { label: "sticky", value: "C" },
+      { label: "block", value: "D" },
+    ],
+  },
+];
--- project2-quiz/src/routes/+page.svelte
+++ project2-quiz/src/routes/+page.svelte
@@ -1,2 +1,36 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+  import { quizQuestions } from "../data/questions";
+</script>
+
+<svelte:head>
+  <title>Quiz</title>
+</svelte:head>
+<main>
+  <article>
+    <h1>Web Development Quiz Demo</h1>
+    <article class="Quiz--question">
+      {#each quizQuestions as question}
+        <h2>Which of the following is not a position value</h2>
+        <ul>
+          {#each question.choices as choice}
+            <li>
+              <label
+                ><input
+                  type="radio"
+                  bind:group={question.selected}
+                  value={choice.value}
+                />{choice.label}</label
+              >
+            </li>
+          {/each}
+        </ul>
+      {/each}
+    </article>
+  </article>
+</main>
+
+<style>
+  ul {
+    list-style-type: upper-alpha;
+  }
+</style>
