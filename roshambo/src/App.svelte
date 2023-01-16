fc27413ee576407fbb610b4cd3d51078e0f6820c
Paul Adams <ghusiyy@gmail.com>
2023-01-16 13:36:21 -0800

    remove boilerplate code

--- roshambo/src/App.svelte
+++ roshambo/src/App.svelte
@@ -1,30 +1,15 @@
 <script lang="ts">
-  import svelteLogo from './assets/svelte.svg'
-  import Counter from './lib/Counter.svelte'
+  import RoshamboGame from "./lib/RoshamboGame.svelte";
 </script>
 
 <main>
-  <div>
-    <a href="https://vitejs.dev" target="_blank" rel="noreferrer"> 
-      <img src="/vite.svg" class="logo" alt="Vite Logo" />
-    </a>
-    <a href="https://svelte.dev" target="_blank" rel="noreferrer"> 
-      <img src={svelteLogo} class="logo svelte" alt="Svelte Logo" />
-    </a>
-  </div>
-  <h1>Vite + Svelte</h1>
+  <h1>Roshambo</h1>
 
   <div class="card">
-    <Counter />
+    <RoshamboGame />
   </div>
 
-  <p>
-    Check out <a href="https://github.com/sveltejs/kit#readme" target="_blank" rel="noreferrer">SvelteKit</a>, the official Svelte app framework powered by Vite!
-  </p>
-
-  <p class="read-the-docs">
-    Click on the Vite and Svelte logos to learn more
-  </p>
+  <p class="read-the-docs">how to play/p></p>
 </main>
 
 <style>
---
+++ roshambo/src/lib/RoshamboGame.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  // TODO: implement roshambo
+</script>
+
+<div class="Roshambo-game">
+  <div class="Left-side">a</div>
+  <div class="Result-display">b</div>
+  <div class="Right-side">c</div>
+</div>
