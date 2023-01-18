c6ee2d888262784c3ffb5766d9d21452aaf98d86
Paul Adams <ghusiyy@gmail.com>
2023-01-17 19:12:39 -0800

    create grid card

--- project3-roshambo/src/lib/RoshamboGame.svelte
+++ project3-roshambo/src/lib/RoshamboGame.svelte
@@ -3,7 +3,19 @@
 </script>
 
 <div class="Roshambo-game">
-  <div class="game-grid Left-side">a</div>
+  <div class="game-grid Left-side">
+    <div class="Shape-wrapper">
+      <div class="Shape">R</div>
+    </div>
+
+    <div class="Selected-wrapper">
+      <div class="Shape">P</div>
+      <div class="Selected-container">selected</div>
+    </div>
+    <div class="Shape-wrapper">
+      <div class="Shape">S</div>
+    </div>
+  </div>
   <div class="Result-display">b</div>
   <div class="game-grid Right-side">c</div>
 </div>
@@ -16,10 +28,37 @@
     display: flex;
   }
   .game-grid {
+    display: grid;
+    grid-template-columns: 1fr 2fr 1fr;
     flex: 1;
     width: 500px;
     height: var(--height);
     outline: 1px solid lightgray;
+  }
+
+  .Shape-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: end;
+    align-items: center;
+  }
+  .Selected-wrapper {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between;
+    align-items: center;
+    height: 100%;
+  }
+  .Selected-container {
+    outline: 1px solid black;
+    height: 60%;
+    margin: 0px 4px 4px 4px;
+  }
+  .Shape {
+    outline: 1px solid black;
+    height: 40px;
+    width: 40px;
+    border-radius: 50%;
   }
   .Result-display {
     flex: 2;
