883054534c1cd4d74d77d4179fcddcbc4117f404
Paul Adams <ghusiyy@gmail.com>
2023-01-20 00:07:43 -0800

    init Notes app

---
+++ project4-notes/src/App.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import NotesArea from "./lib/NotesArea.svelte";
+</script>
+
+<main>
+  <h2>Notes Project</h2>
+  <div class="card">
+    <NotesArea />
+  </div>
+</main>
+
+<style>
+  h2 {
+    text-align: center;
+  }
+</style>
---
+++ project4-notes/src/lib/NotesArea.svelte
@@ -0,0 +1,35 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  // TODO: implement notes app
+  // using local storage to store notes data
+  let area: HTMLTextAreaElement;
+  $: note = "";
+  onMount(() => {
+    area.focus();
+  });
+</script>
+
+<div class="Notes-area">
+  <div class="Area-pane">
+    <label for="notes">Create, edit, or view notes</label>
+    <textarea id="notes" bind:this={area} bind:value={note} />
+  </div>
+  <div class="Preview-pane">
+    <pre>{note}</pre>
+  </div>
+</div>
+
+<style>
+  .Notes-area {
+    display: flex;
+    flex-direction: column;
+  }
+  .Area-pane {
+    display: flex;
+    flex-direction: column;
+  }
+  .Preview-pane {
+    margin-top: 10px;
+  }
+</style>
