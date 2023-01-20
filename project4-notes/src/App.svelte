ed003b41a20dae1df5f330fbeab6f8c779da916a
Paul Adams <ghusiyy@gmail.com>
2023-01-20 14:41:14 -0800

    notes list event

--- project4-notes/src/App.svelte
+++ project4-notes/src/App.svelte
@@ -1,10 +1,32 @@
 <script lang="ts">
   import NotesArea from "./lib/NotesArea.svelte";
+  import NotesList from "./lib/NotesList.svelte";
+  $: notes = [
+    {
+      id: 0,
+      title: "note #1",
+      body: "",
+    },
+    {
+      id: 1,
+      title: "note #2",
+      body: "",
+    },
+    {
+      id: 2,
+      title: "note #3",
+      body: "",
+    },
+  ];
 </script>
 
 <main>
   <h2>Notes Project</h2>
   <div class="card">
+    <NotesList
+      {notes}
+      on:selectNote={(event) => console.log(event.detail.note)}
+    />
     <NotesArea />
   </div>
 </main>
---
+++ project4-notes/src/lib/NotesList.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import { createEventDispatcher } from "svelte";
+  const dispatch = createEventDispatcher();
+  export let notes = [];
+</script>
+
+<div class="Notes-list">
+  {#each notes as note}
+    <div on:click={() => dispatch("selectNote", { note })} aria-hidden="true">
+      {note.title}
+    </div>
+  {/each}
+</div>
