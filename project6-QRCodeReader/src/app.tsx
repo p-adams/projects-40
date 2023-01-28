019bbbe774436336f8a785e57c526d6b4616b7f6
Paul Adams <ghusiyy@gmail.com>
2023-01-28 12:16:39 -0800

    init project 6: QR code reader

---
+++ project6-QRCodeReader/src/app.tsx
@@ -0,0 +1,12 @@
+import { useState } from "preact/hooks";
+
+import "./app.css";
+
+export function App() {
+  return (
+    <>
+      <h1>QR Code Reader</h1>
+      <div class="card"></div>
+    </>
+  );
+}
