a6886451e06dacbc1c7b492b5817ec2f592e7f54
Paul Adams <ghusiyy@gmail.com>
2023-01-30 02:02:28 -0800

    display current weather

--- project7-weather/src/App.svelte
+++ project7-weather/src/App.svelte
@@ -1,13 +1,40 @@
 <script lang="ts">
   import { onMount } from "svelte";
+  interface ApiResponse {
+    current_weather: {
+      temperature: number;
+      windspeed: number;
+      winddirection: number;
+    };
+
+    elevation: number;
+    generationtime_ms: number;
+    hourly: { time: Array<string>; temperature_2m: Array<number> };
+    hourly_units: { time: string; temperature_2m: string };
+    latitude: number;
+    longitude: number;
+    timezone: string;
+    timezone_abbreviation: string;
+    utc_offset_seconds: number;
+  }
 
   const API_BASE_URL = (
-    params = { latitude: 37.55, longitude: -121.99, hourly: "temperature_2m" }
+    params = {
+      latitude: 37.55,
+      longitude: -121.99,
+      hourly: "temperature_2m",
+      temperature_unit: "fahrenheit",
+    }
   ) =>
-    `https://api.open-meteo.com/v1/forecast?latitude=${params.latitude}&longitude=${params.longitude}&hourly=${params.hourly}`;
+    `https://api.open-meteo.com/v1/forecast?latitude=${params.latitude}&longitude=${params.longitude}&hourly=${params.hourly}&current_weather=true&temperature_unit=${params.temperature_unit}`;
+
+  $: weatherData = null as ApiResponse;
   onMount(async () => {
     try {
-      const response = await fetch(API_BASE_URL()).then((res) => res.json());
+      const response: ApiResponse = await fetch(API_BASE_URL()).then((res) =>
+        res.json()
+      );
+      weatherData = response;
       console.log(response);
     } catch (error) {
       console.log(error);
@@ -18,7 +45,12 @@
 <main>
   <h1>Weather</h1>
 
-  <div class="card" />
+  <div class="card">
+    <h3>Current Weather</h3>
+    <div>
+      <div>{weatherData?.current_weather?.temperature}</div>
+    </div>
+  </div>
 </main>
 
 <style>
