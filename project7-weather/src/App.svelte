721305c68d02a77165c2f9dfa0b846a7584a13ac
Paul Adams <ghusiyy@gmail.com>
2023-01-30 23:05:01 -0800

    extract types out to namespace

--- project7-weather/src/App.svelte
+++ project7-weather/src/App.svelte
@@ -1,60 +1,33 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  interface ApiResponse {
-    current_weather: {
-      temperature: number;
-      windspeed: number;
-      winddirection: number;
-    };
 
-    elevation: number;
-    generationtime_ms: number;
-    hourly: { time: Array<string>; temperature_2m: Array<number> };
-    hourly_units: { time: string; temperature_2m: string };
-    latitude: number;
-    longitude: number;
-    timezone: string;
-    timezone_abbreviation: string;
-    utc_offset_seconds: number;
-    temperature_unit?: string;
-  }
-
-  enum Hourly {
-    temperature_2m = "temperature_2m",
-  }
-
-  type HourlyParameter = {
-    hourly?: Hourly;
-  };
-
-  type BaseUrlParams = Pick<
-    ApiResponse,
-    "latitude" | "longitude" | "temperature_unit"
-  > &
-    HourlyParameter;
-  const API_BASE_URL = (params: BaseUrlParams) =>
+  const WEATHER_API_BASE_URL = (params: Lib.BaseUrlParams) =>
     `https://api.open-meteo.com/v1/forecast?latitude=${params.latitude}&longitude=${params.longitude}&hourly=${params.hourly}&current_weather=true&temperature_unit=${params.temperature_unit}`;
 
-  $: weatherData = null as ApiResponse;
+  $: weatherData = null as Lib.WeatherApiResponse;
+
+  async function fetchJsonData(path: string): Promise<any> {
+    return fetch(path).then((res) => res.json());
+  }
+
   onMount(async () => {
     try {
       const query = "Addis Ababa, Ethiopia";
-      const geo = await fetch(
+
+      const geo = await fetchJsonData(
         `https://geocode.search.hereapi.com/v1/geocode?q=${query}&apiKey=${
           import.meta.env.VITE_API_KEY
         }`
-      ).then((res) => res.json());
+      );
       const { position } = geo.items[0];
-      const response: ApiResponse = await fetch(
-        API_BASE_URL({
+      weatherData = await fetchJsonData(
+        WEATHER_API_BASE_URL({
           latitude: position.lat,
           longitude: position.lng,
-          hourly: Hourly.temperature_2m,
+          hourly: Lib.Hourly.temperature_2m,
           temperature_unit: "fahrenheit",
         })
-      ).then((res) => res.json());
-
-      weatherData = response;
+      );
     } catch (error) {
       console.log(error);
     }
