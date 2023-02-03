19b093be1c8a91321654b7cc18fc23c91102d88d
Paul Adams <ghusiyy@gmail.com>
2023-02-02 21:57:34 -0800

    add home route

--- project8-e-commerce/src/lib/server/index.ts
+++ project8-e-commerce/src/lib/server/index.ts
@@ -5,8 +5,20 @@ async function run() {
 		port: 3000,
 		host: 'localhost'
 	});
+	server.route({
+		method: 'GET',
+		path: '/',
+		handler: (request, h) => {
+			return 'Hello World!';
+		}
+	});
 	await server.start();
 	console.log(`Server running on ${server.info.host}`);
 }
 
-export default run();
+process.on('unhandledRejection', (err) => {
+	console.log(err);
+	process.exit(1);
+});
+
+run();
