85898eef28b14fcf70aeadeb465e7557ae4509d9
Paul Adams <ghusiyy@gmail.com>
2023-02-02 22:22:37 -0800

    init db

---
+++ project8-e-commerce/src/lib/server/db.ts
@@ -0,0 +1,10 @@
+import { Database } from 'sqlite3';
+const db = new Database(':memory:', async (err) => {
+	if (err) {
+		return console.error(err.message);
+	}
+
+	console.log('Connected to DB');
+});
+
+db.serialize(async () => {});
