83a6c44dff121a3636c828b767a61358b2e4326b
Paul Adams <ghusiyy@gmail.com>
2023-02-04 01:23:08 -0800

    add types

--- project8-e-commerce/src/lib/types/index.ts
+++ project8-e-commerce/src/lib/types/index.ts
@@ -3,12 +3,42 @@ export interface ShoppingCart {
 }
 
 interface ProductVariant {
-	size: string;
-	color: string;
+	barcode: string;
+	productId: number;
+	// id used to associate an inventory item
+	inventoryId: number;
+	name: string;
+	cost: number;
+	sku: string;
+}
+
+interface ProductOptions {
+	// id used to associate a variant
+	productId: number;
+	name: string; // size, color, etc
+	values: Array<string>;
+	quantity: number;
 }
 
 export interface Product {
+	productId: number;
+	name: string;
 	description: string;
-	cost: number;
+	options: Array<ProductOptions>;
 	variants: Array<ProductVariant>;
+	vendor: string;
+}
+
+export interface Inventory {
+	unitCost: number;
+	origin: string; // where item was made
+	inventoryId: number;
+	sku: string;
+	location: Location;
+}
+
+export interface Location {
+	inventoryId: number;
+	quantity: number;
+	address: any;
 }
