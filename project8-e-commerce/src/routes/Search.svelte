9834a50cda1ad19248ff69a5c6abe5f11a7d22a7
Paul Adams <ghusiyy@gmail.com>
2023-03-04 00:12:14 -0800

    move search into component

---
+++ project8-e-commerce/src/routes/Search.svelte
@@ -0,0 +1,21 @@
+<script lang="ts">
+	import { createEventDispatcher } from 'svelte';
+	const dispatch = createEventDispatcher();
+	let timeoutId: NodeJS.Timer;
+	function search(e: KeyboardEvent & { currentTarget: EventTarget & HTMLInputElement }) {
+		const $input = e?.currentTarget?.value;
+		clearTimeout(timeoutId);
+		timeoutId = setTimeout(() => {
+			dispatch('search', { value: $input });
+		}, 500);
+	}
+</script>
+
+<div class="search">
+	<input on:keyup={(e) => search(e)} />
+	<!-- TODO impl dropdown ul>
+        <li>a</li>
+        <li>b</li>
+        <li>c</li>
+    </ul>-->
+</div>
