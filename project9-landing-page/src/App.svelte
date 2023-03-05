29b49b50981286af509eee89628e5c86f2e33fcc
Paul Adams <ghusiyy@gmail.com>
2023-03-04 23:38:22 -0800

    rescope landing page

--- project9-landing-page/src/App.svelte
+++ project9-landing-page/src/App.svelte
@@ -1,29 +1,24 @@
 <script lang="ts">
-  import svelteLogo from "./assets/svelte.svg";
+  import geo from "./assets/geo.jpg";
 </script>
 
 <main>
-  <div>
-    <a href="#/" target="_blank" rel="noreferrer">
-      <img src={svelteLogo} class="logo TeeTajir" alt="TeeTajir Logo" />
-    </a>
+  <div class="banner">
+    <img src={geo} class="banner-image" alt="Banner" />
   </div>
-  <h1>TeeTajir</h1>
+  <h1 class="title">Sixty Issues</h1>
 
   <div class="card" />
 </main>
 
 <style>
-  .logo {
-    height: 6em;
-    padding: 1.5em;
-    will-change: filter;
-    transition: filter 300ms;
+  .banner {
+    height: 100px;
+    position: relative;
   }
-  .logo:hover {
-    filter: drop-shadow(0 0 2em #646cffaa);
-  }
-  .logo.TeeTajir:hover {
-    filter: drop-shadow(0 0 2em #ff3e00aa);
+  .banner-image {
+    height: 100%;
+    width: 100%;
+    object-fit: cover;
   }
 </style>
