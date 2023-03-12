b66c6ced5bba09c530199b6a2889eb93a439e0a6
Paul Adams <ghusiyy@gmail.com>
2023-03-12 00:42:21 -0800

    display slde selections

--- project15-nasi-builder/src/routes/+page.svelte
+++ project15-nasi-builder/src/routes/+page.svelte
@@ -1,8 +1,65 @@
+<script lang="ts">
+  import { afterUpdate } from "svelte";
+
+  type Side = { label: string; value: string };
+
+  let sides: Array<Side> = [
+    { label: "Chili Sauce", value: "chili_sauce" },
+    { label: "Fried Anchovies", value: "fried_anchovies" },
+    { label: "Roasted Peanuts", value: "roasted_peanuts" },
+    { label: "Cucumber", value: "cucumber" },
+    { label: "Fried Chicken", value: "fried_chicken" },
+  ];
+
+  let selectedSides: Array<Side> = [];
+  const removeSide = (side: Side) =>
+    (selectedSides = selectedSides.filter(
+      (sside) => sside.value !== side.value
+    ));
+</script>
+
 <svelte:head>
   <title>Home</title>
   <meta name="description" content="Home" />
 </svelte:head>
 <section>
-  <h1>Nasi Lemak Builder</h1>
-  <div>app goes here</div>
+  <h1>Nasi Builder</h1>
+
+  <div>
+    <div class="selected-sides">
+      {#each selectedSides as side}
+        <div class="chip">
+          <span>{side.label}</span>
+          <button on:click={() => removeSide(side)}>X</button>
+        </div>
+      {/each}
+    </div>
+    <select name="sides" bind:value={selectedSides} multiple>
+      {#each sides as side}
+        <option value={side}>{side.label}</option>
+      {/each}
+    </select>
+  </div>
 </section>
+
+<style>
+  .selected-sides {
+    min-height: 50px;
+    display: flex;
+    margin-bottom: 10px;
+  }
+  .chip {
+    display: flex;
+    justify-content: end;
+    align-items: center;
+    width: fit-content;
+    border: 1px solid lightgray;
+    padding: 10px;
+    border-radius: 30px;
+    font-size: small;
+  }
+  .chip button {
+    font-size: 10px;
+    margin-left: 4px;
+  }
+</style>
