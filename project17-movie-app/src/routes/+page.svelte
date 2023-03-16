8db54a11a60ffb76f9ab9d6b45caad43d4fca939
Paul Adams <ghusiyy@gmail.com>
2023-03-16 00:35:23 -0700

    project 17: init movie app

---
+++ project17-movie-app/src/routes/+page.svelte
@@ -0,0 +1,16 @@
+<svelte:head>
+  <title>Home</title>
+  <meta name="description" content="home" />
+</svelte:head>
+<section>
+  <h1>Movie App</h1>
+  <div>
+    <video
+      controls
+      src="https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4"
+      width="600"
+    >
+      <track default kind="captions" srclang="en" src="" />
+    </video>
+  </div>
+</section>
