1dc7342eea5584785c6519faca59be0989961e86
Paul Adams <ghusiyy@gmail.com>
2023-03-17 00:05:22 -0700

    add simple error handling

--- project17-skate-clips/src/routes/+page.svelte
+++ project17-skate-clips/src/routes/+page.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   type Clip = { title: string; featured: string; src: string };
+  const FALLBACK_CLIP =
+    "https://archive.org/download/BigBuckBunny_124/Content/big_buck_bunny_720p_surround.mp4";
   const clips: Array<Clip> = [
     {
       title: "2411-QJ02 HD24",
@@ -7,7 +9,7 @@
       src: "https://i.imgur.com/cQZyxV0.mp4",
     },
   ];
-  let selectedClip: Clip | null = clips[0];
+  let selectedClip: Clip | undefined = clips[0];
 </script>
 
 <svelte:head>
@@ -16,19 +18,24 @@
 </svelte:head>
 <section>
   <h1>Skate Clips</h1>
+
   <div class="Clips-app">
-    <video controls src={selectedClip?.src ?? ""} width="600">
-      <track default kind="captions" srclang="en" src="" />
-    </video>
-    <div>
-      <ul>
-        {#each clips as clip}
-          <li on:click={() => (selectedClip = clip)} aria-hidden="true">
-            {clip.featured} - {clip.title}
-          </li>
-        {/each}
-      </ul>
-    </div>
+    {#if clips.length > 0}
+      <video controls src={selectedClip?.src ?? FALLBACK_CLIP} width="600">
+        <track default kind="captions" srclang="en" src="" />
+      </video>
+      <div>
+        <ul>
+          {#each clips as clip}
+            <li on:click={() => (selectedClip = clip)} aria-hidden="true">
+              {clip.featured} - {clip.title}
+            </li>
+          {/each}
+        </ul>
+      </div>
+    {:else}
+      <div>No videos found :(</div>
+    {/if}
   </div>
 </section>
 
