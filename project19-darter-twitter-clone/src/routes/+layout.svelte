42bc81c72e2e2fd61f45d2a712d2f90ce432a984
Paul Adams <ghusiyy@gmail.com>
2023-03-23 23:54:33 -0700

    add global css and color palette

--- project19-darter-twitter-clone/src/routes/+layout.svelte
+++ project19-darter-twitter-clone/src/routes/+layout.svelte
@@ -7,15 +7,8 @@
   <slot />
 </main>
 
-<style>
-  /**
-    COLOR PALETTE:
-    #003049 - This is a dark navy blue color that can be used for headings and buttons.
-    #F77F00 - This is a warm orange color that can be used for accents and calls to action.
-    #D62828 - This is a deep red color that can be used sparingly for emphasis.
-    #EAEAEA - This is a light gray color that can be used for backgrounds or as a secondary color for text.
-    #FFFFFF - This is a pure white color that can be used for backgrounds and text.
-  */
+<style global>
+  @import "../app.css";
   main {
     margin: 0px 40px 0px 40px;
   }
