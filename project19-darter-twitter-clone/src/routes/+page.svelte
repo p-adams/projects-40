b1c212d449fe08bbacc54363388db03fad5c8d4d
Paul Adams <ghusiyy@gmail.com>
2023-03-29 23:42:47 -0700

    add feed card layout and profile route

--- project19-darter-twitter-clone/src/routes/+page.svelte
+++ project19-darter-twitter-clone/src/routes/+page.svelte
@@ -18,7 +18,21 @@
     <h1 class="feed-title">Darts</h1>
     <div>
       {#each dartFeed as feedItem}
-        <div class="feed-card">{feedItem.text}</div>
+        <div class="feed-card">
+          <div>
+            <img
+              src="https://via.placeholder.com/75"
+              alt={`user-${feedItem.username}`}
+            />
+          </div>
+          <div class="content">
+            <div class="top-content">
+              <a href={`/profile/${feedItem.username}`}>{feedItem.username}</a>
+              <div>{feedItem.date.toLocaleString()}</div>
+            </div>
+            <div class="main-content">{feedItem.text}</div>
+          </div>
+        </div>
       {/each}
     </div>
   </section>
@@ -34,6 +48,24 @@
     color: var(--darkNavy);
     margin-bottom: 10px;
     border-bottom: 1px solid var(--lightGray);
+  }
+  .feed-card {
+    display: flex;
+    gap: 10px;
+  }
+  .feed-card:not(:last-child) {
+    border-bottom: 1px solid var(--lightGray);
+  }
+  .content {
+    width: 100%;
+    margin: 2px 12px 0px 0px;
+  }
+  .top-content {
+    display: flex;
+    justify-content: space-between;
+  }
+  .main-content {
+    margin-top: 4px;
   }
 
   section {
