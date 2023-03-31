40352cf6664787f06a2c34fdf555869ce8bc05e2
Paul Adams <ghusiyy@gmail.com>
2023-03-30 23:49:51 -0700

    init feed card

---
+++ project19-darter-twitter-clone/src/lib/FeedCard.svelte
@@ -0,0 +1,15 @@
+<div class="feed-card">
+  <div>
+    <img
+      src="https://via.placeholder.com/75"
+      alt={`user-${feedItem.username}`}
+    />
+  </div>
+  <div class="content">
+    <div class="top-content">
+      <a href={`/profile/${feedItem.username}`}>{feedItem.username}</a>
+      <div>{feedItem.date.toLocaleString()}</div>
+    </div>
+    <div class="main-content">{feedItem.text}</div>
+  </div>
+</div>
