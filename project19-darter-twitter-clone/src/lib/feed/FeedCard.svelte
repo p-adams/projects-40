98831e7aabf99977eb864fee123740cd8a35b6d2
Paul Adams <ghusiyy@gmail.com>
2023-04-21 00:20:35 -0700

    fix feed item type

--- project19-darter-twitter-clone/src/lib/feed/FeedCard.svelte
+++ project19-darter-twitter-clone/src/lib/feed/FeedCard.svelte
@@ -1,17 +1,20 @@
 <script lang="ts">
-  export let dart: Lib.Dart;
+  export let feedItem: Lib.Dart;
 </script>
 
 <div class="feed-card">
   <div>
-    <img src="https://via.placeholder.com/75" alt={`user-${dart.username}`} />
+    <img
+      src="https://via.placeholder.com/75"
+      alt={`user-${feedItem.username}`}
+    />
   </div>
   <div class="content">
     <div class="top-content">
-      <a href={`/profile/${dart.username}`}>{dart.username}</a>
-      <div>{dart.date.toLocaleString()}</div>
+      <a href={`/profile/${feedItem.username}`}>{feedItem.username}</a>
+      <div>{feedItem.date.toLocaleString()}</div>
     </div>
-    <div class="main-content">{dart.text}</div>
+    <div class="main-content">{feedItem.text}</div>
   </div>
 </div>
 
