874bf892e0f419d6cc415802c8271f3043f5b55b
Paul Adams <ghusiyy@gmail.com>
2023-05-04 23:01:47 -0700

    init basic types

---
+++ project20-survey-app/src/lib/types.d.ts
@@ -0,0 +1,19 @@
+namespace Lib {
+  type Question = { label: string; value: string };
+  type Questions = Array<Questions>;
+  // response can be array of questions when multiple responses are available
+  type SurveyResponse = Question | Questions;
+  interface SurveyQuestions {
+    id: string;
+    name: string;
+    questions: Questions;
+    response: SurveyResponse;
+  }
+  interface SurveyCategory {
+    id: string;
+    name: string;
+    description: string;
+    assets?: Array<string>;
+    surveyQuestions: Array<SurveyQuestions>;
+  }
+}
