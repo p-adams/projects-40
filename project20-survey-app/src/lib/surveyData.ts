81ca12ea95b8e10e0c876676b3e8e96fa1b1bf58
Paul Adams <ghusiyy@gmail.com>
2023-05-05 11:38:37 -0700

    add mock description

--- project20-survey-app/src/lib/surveyData.ts
+++ project20-survey-app/src/lib/surveyData.ts
@@ -1,7 +1,12 @@
 import { nanoid } from "nanoid";
 
 export const data: Array<Lib.SurveyCategory> = [
-  { id: nanoid(), name: "Demographics" },
+  {
+    id: nanoid(),
+    name: "Demographics",
+    description:
+      "Gender, age, ethnicity, location, education, maritial status, and stance",
+  },
 
   { id: nanoid(), name: "Content" },
   // example content categories { id: nanoid(), name: "Video" },
