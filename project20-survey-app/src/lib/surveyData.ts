1f53503b78af58d888f6e2dcbe73d2ce7b19fb12
Paul Adams <ghusiyy@gmail.com>
2023-05-05 11:33:59 -0700

    update mock survey data

--- project20-survey-app/src/lib/surveyData.ts
+++ project20-survey-app/src/lib/surveyData.ts
@@ -1,8 +1,11 @@
 import { nanoid } from "nanoid";
 
 export const data: Array<Lib.SurveyCategory> = [
-  { id: nanoid(), name: "Personal Information" },
-  { id: nanoid(), name: "Video" },
-  { id: nanoid(), name: "Photography" },
-  { id: nanoid(), name: "Social Media" },
+  { id: nanoid(), name: "Demographics" },
+
+  { id: nanoid(), name: "Content" },
+  // example content categories { id: nanoid(), name: "Video" },
+  // { id: nanoid(), name: "Photography" },
+  // { id: nanoid(), name: "Social Media" },
+  { id: nanoid(), name: "Products" },
 ];
