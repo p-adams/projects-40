332fdcebd7da981e8bf89fe6debb75e88bec4e82
Paul Adams <ghusiyy@gmail.com>
2023-05-11 14:15:44 -0700

    init survey category sidebar

---
+++ project20-survey-app/src/lib/SurveyCategorySidebar.svelte
@@ -0,0 +1,6 @@
+<script lang="ts">
+  import { onMount } from "svelte";
+
+  export let data: Array<Lib.SurveyCategory> = [];
+  onMount(() => console.log(data));
+</script>
---
+++ project20-survey-app/src/routes/+layout.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import SurveyCategorySidebar from "$lib/SurveyCategorySidebar.svelte";
+  import { data } from "../lib/surveyData";
+</script>
+
+<SurveyCategorySidebar {data} />
+<main>
+  <slot />
+</main>
