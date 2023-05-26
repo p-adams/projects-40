59d22115194609ca699dd04ffed5988d295c4e7f
Paul Adams <ghusiyy@gmail.com>
2023-05-25 23:21:40 -0700

    add some content survey questions

--- project20-survey-app/src/lib/surveyStore.ts
+++ project20-survey-app/src/lib/surveyStore.ts
@@ -2,12 +2,10 @@ import { writable } from "svelte/store";
 
 const surveyStore = writable<{ [key: string]: any }>({});
 
-// use for single selection
-
 function updateSurvey(key: string, value: string) {
   surveyStore.update((currentSelectedValues) => {
     const updatedSelectedValues = { ...currentSelectedValues };
-    updatedSelectedValues[key] = value; // Update the selected value for the specific key
+    updatedSelectedValues[key] = value;
     return updatedSelectedValues;
   });
 }
