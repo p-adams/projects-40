8fd6f13223a2b6bc83ae4d2c41066f3d9c27875b
Paul Adams <ghusiyy@gmail.com>
2023-06-16 00:01:19 -0700

    add results route

--- project20-survey-app/src/routes/+layout.svelte
+++ project20-survey-app/src/routes/+layout.svelte
@@ -3,6 +3,14 @@
   import { data } from "../lib/surveyData";
 </script>
 
+<header>
+  <nav>
+    <ul>
+      <li><a href="/">Home</a></li>
+      <li><a href="/results">View Results</a></li>
+    </ul>
+  </nav>
+</header>
 <div class="page">
   <SurveyCategorySidebar {data} />
   <main>
---
+++ project20-survey-app/src/routes/results/+page.svelte
@@ -0,0 +1,10 @@
+<svelte:head>
+  <title>Results</title>
+  <meta name="Description" content="Results" />
+</svelte:head>
+<main>
+  <section>
+    <h1>Survey Results</h1>
+    <div>Coming Soon!</div>
+  </section>
+</main>
