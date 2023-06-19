1b9c4bb7f41d6aecb9d82bd7bac9767251c3fc2e
Paul Adams <ghusiyy@gmail.com>
2023-06-19 00:24:32 -0700

    add function and class to distinguish first highest response score

--- project20-survey-app/src/lib/ResultGroups.svelte
+++ project20-survey-app/src/lib/ResultGroups.svelte
@@ -2,6 +2,38 @@
   export let data: Lib.SurveyCategory;
   export let responses: Lib.SurveyResult;
   const categoryName = (name: string) => name.replaceAll(/\s/g, "");
+  function maxResponse(responses: Record<string, number>): {
+    key: string;
+    value: number;
+  } | null {
+    const values = Object.values(responses);
+    const highestValue = Math.max(...values);
+    const highestKey = Object.keys(responses).find(
+      (key) => responses[key] === highestValue
+    );
+    if (!highestKey) {
+      return null;
+    }
+
+    return { key: highestKey, value: highestValue };
+  }
+  function maxResponseClass(
+    responses: any,
+    category: Lib.SurveyQuestionGroup,
+    subcategoryValue: string
+  ) {
+    const categoryResponses = responses[categoryName(category.name)];
+    const $max = maxResponse(categoryResponses);
+    if (!$max) {
+      return "";
+    }
+    const maxResponseValue = $max.value;
+    const maxResponseKey = $max.key;
+
+    return maxResponseValue && maxResponseKey === subcategoryValue
+      ? "max-response"
+      : "";
+  }
 </script>
 
 <section>
@@ -12,10 +44,15 @@
       {#if category.name}
         <li>
           <h3 class="category-name">{category.name}</h3>
-
           <ul>
             {#each category.options as subcategory}
-              <li class="result-item">
+              <li
+                class={`result-item ${maxResponseClass(
+                  responses,
+                  category,
+                  subcategory.value
+                )}`}
+              >
                 <div class="category-name sub">{subcategory.label}</div>
                 <div class="score">
                   {responses[categoryName(category.name)][subcategory.value]}
@@ -35,5 +72,8 @@
   }
   .score {
     margin-left: 10px;
+  }
+  .max-response {
+    background-color: aquamarine;
   }
 </style>
