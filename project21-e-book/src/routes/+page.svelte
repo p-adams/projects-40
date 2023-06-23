3e76eab3a45686bec29566b1cb49d1fa6b575f7b
Paul Adams <ghusiyy@gmail.com>
2023-06-23 00:54:51 -0700

    display download table

--- project21-e-book/src/routes/+page.svelte
+++ project21-e-book/src/routes/+page.svelte
@@ -1,5 +1,14 @@
 <script lang="ts">
   export let data;
+  function formatData(format: any) {
+    return Object.keys(format);
+  }
+  function getFormatLink(
+    format: Record<string, string | undefined>,
+    key: string
+  ): string | undefined {
+    return format[key];
+  }
 </script>
 
 <svelte:head>
@@ -26,14 +35,65 @@
           </div>
           <div class="Item--secondary">
             <div><img src={result.formats["image/jpeg"]} alt="" /></div>
-            <div>download links table</div>
+            <div>
+              <h2>Download</h2>
+              <div class="Download--table">
+                <div class="th">Format</div>
+                <div class="th">Link</div>
+                {#each formatData(result.formats) as key}
+                  <div class="Download--row">
+                    <div class="td">{key}</div>
+                    <div class="td">
+                      <a
+                        href={getFormatLink(result.formats, key)}
+                        target="_blank">{getFormatLink(result.formats, key)}</a
+                      >
+                    </div>
+                  </div>
+                {/each}
+              </div>
+            </div>
           </div>
           <div class="Item--tertiary">
-            <div>bookshelves</div>
-            <div>languages</div>
-            <div>subjects</div>
-            <div>translators</div>
-            <div>download count</div>
+            <div>
+              Subjects: <ul>
+                {#each result.subjects as subject}
+                  <li>{subject}</li>
+                {/each}
+              </ul>
+            </div>
+            <div>
+              Bookshelves: {#if !!result.bookshelves.length}
+                <ul>
+                  {#each result.bookshelves as bookshelf}
+                    <li>{bookshelf}</li>
+                  {/each}
+                </ul>
+              {:else}
+                {"—"}
+              {/if}
+            </div>
+            <div>
+              Languages: <ul>
+                {#each result.languages as language}
+                  <li>{language}</li>
+                {/each}
+              </ul>
+            </div>
+            <div>
+              Translators:
+              {#if !!result.translators.length}
+                <ul>
+                  {#each result.translators as translator}
+                    <li>{translator}</li>
+                  {/each}
+                </ul>
+              {:else}
+                {"—"}
+              {/if}
+            </div>
+
+            <div>Download: {result.download_count}</div>
           </div>
         </li>
       {/each}
@@ -44,5 +104,21 @@
 <style>
   .Item--title {
     font-weight: bold;
+  }
+  .Item--secondary {
+    display: flex;
+    gap: 16px;
+  }
+  .Download--table {
+    display: grid;
+    grid-template-columns: minmax(150px, 200px) 1fr;
+  }
+  .Download--row {
+    display: contents;
+  }
+  .th,
+  .td {
+    border: 1px solid gray;
+    padding: 8px;
   }
 </style>
