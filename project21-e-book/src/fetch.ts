46e23620ce05c289551062e34d062af24406e7ee
Paul Adams <ghusiyy@gmail.com>
2023-06-26 23:52:49 -0700

    pass page number to fetchBooks

--- project21-e-book/src/fetch.ts
+++ project21-e-book/src/fetch.ts
@@ -1,8 +1,8 @@
 import { MOCK_DATA } from "./MOCK_DATA";
 
-export function fetchBooks() {
+export function fetchBooks(page: number = 1) {
   /**
-     *  return await fetch(`${import.meta.env.VITE_ENDPOINT}/books`)
+     *  return await fetch(`${import.meta.env.VITE_ENDPOINT}/books/?page=${page}`)
     .then(($r) => $r.json())
     .catch((err) => console.error(err));
     */
