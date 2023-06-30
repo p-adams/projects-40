9eafe7cd94035bf8ec44c5f151d835970b65b294
Paul Adams <ghusiyy@gmail.com>
2023-06-30 00:39:34 -0700

    update page title

--- project22-photo-share/src/routes/+page.svelte
+++ project22-photo-share/src/routes/+page.svelte
@@ -1,7 +1,8 @@
 <script lang="ts">
 </script>
 
-<h1>Photo Share</h1>
+<h1>Photo Share App</h1>
+<h2>Allowing users to create engaging visual stories</h2>
 
 <p>
   Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation
