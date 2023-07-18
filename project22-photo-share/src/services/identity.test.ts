28812f22c41ca73cdaf308e15496ced0ef216bee
Paul Adams <ghusiyy@gmail.com>
2023-07-18 00:17:18 -0700

    add authentication and registration tests

--- project22-photo-share/src/services/identity.test.ts
+++ project22-photo-share/src/services/identity.test.ts
@@ -3,18 +3,39 @@ import { IdentityService } from "./identity";
 
 describe("IdentityService", () => {
   const identityService = new IdentityService();
-  it("#test registration", () => {
+
+  it("#test registration success", () => {
     const registrationResult = identityService.registerUser(
       "john123",
       "password123"
     );
     expect(registrationResult).toBe("User registered successfully.");
   });
-  it("#test authentication", () => {
+  it("#test registration failure", () => {
+    const registrationResult = identityService.registerUser(
+      "john123",
+      "password123"
+    );
+    const registrationResult2 = identityService.registerUser(
+      "john123",
+      "password123"
+    );
+    expect(registrationResult2).toBe("Username already taken.");
+  });
+
+  it("#test authentication success", () => {
     const authenticationResult = identityService.authenticateUser(
       "john123",
       "password123"
     );
     expect(authenticationResult).toBe("Authentication successful.");
+  });
+
+  it("#test authentication failure", () => {
+    const authenticationResult = identityService.authenticateUser(
+      "john1234",
+      "password1234"
+    );
+    expect(authenticationResult).toBe("Invalid username or password.");
   });
 });
