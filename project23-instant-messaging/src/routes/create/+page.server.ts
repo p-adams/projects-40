f1f7f8b8ea5de4af7089d5c1dded5baa9e34f124
Paul Adams <ghusiyy@gmail.com>
2023-09-04 22:41:30 -0700

    init create profile page

--- project23-instant-messaging/src/routes/create/+page.server.ts
+++ project23-instant-messaging/src/routes/create/+page.server.ts
@@ -1,4 +1,11 @@
-export async function load({ request }) {
-  const data = await request.formData();
-  return 200;
-}
+export const actions = {
+  default: async ({ request }) => {
+    const data = Object.fromEntries(await request.formData());
+    const username = data.username;
+    const email = data.email;
+    console.log(username, email);
+    return {
+      result: "SUCCESS",
+    };
+  },
+};
--- project23-instant-messaging/src/routes/create/+page.svelte
+++ project23-instant-messaging/src/routes/create/+page.svelte
@@ -1,3 +1,39 @@
 <form method="POST">
-  <button>Create Profile</button>
+  <label>
+    Username
+    <input type="text" name="username" />
+  </label>
+  <label>
+    Email
+    <input type="email" name="email" />
+  </label>
+  <div class="Create--outer">
+    <button>Create</button>
+  </div>
 </form>
+
+<style>
+  form {
+    display: flex;
+    flex-direction: column;
+    align-items: flex-start;
+    width: fit-content;
+    outline: 1px solid gray;
+    padding: 10px;
+  }
+  label {
+    display: flex;
+    flex-direction: column;
+  }
+  input {
+    margin-bottom: 10px;
+  }
+  .Create--outer {
+    display: flex;
+    justify-content: end;
+  }
+
+  .Create--outer button {
+    width: 60px;
+  }
+</style>
