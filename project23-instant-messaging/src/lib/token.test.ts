a8622f8aff4c8ea36d5b8ff9147a6e24d7618735
Paul Adams <ghusiyy@gmail.com>
2023-09-05 12:43:24 -0700

    add test case

--- project23-instant-messaging/src/lib/token.test.ts
+++ project23-instant-messaging/src/lib/token.test.ts
@@ -2,10 +2,19 @@ import { describe, it, expect } from "vitest";
 import { tokenManager } from "./token";
 
 describe("#Token", () => {
-  const userId = "123";
   it("should generate and return token", () => {
+    const userId = "123";
     const userToken = tokenManager.generateToken(userId) ?? "";
     expect(tokenManager.validateToken(userToken)).toBeTruthy();
+  });
+
+  it("token is invalid when used again ", () => {
+    const userId = "123";
+    const userToken = tokenManager.generateToken(userId) ?? "";
+    // init first usage
+    tokenManager.validateToken(userToken);
+    const secondUsageIsValid = tokenManager.validateToken(userToken);
+    expect(secondUsageIsValid).toBe(false);
   });
 
   it("invalidates token ", () => {
