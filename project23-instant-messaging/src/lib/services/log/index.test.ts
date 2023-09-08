5852792ab71630637b35db403e3a0d716cabf8c2
Paul Adams <ghusiyy@gmail.com>
2023-09-08 00:20:47 -0700

    add log service test

--- project23-instant-messaging/src/lib/services/log/index.test.ts
+++ project23-instant-messaging/src/lib/services/log/index.test.ts
@@ -1,7 +1,20 @@
 import { beforeEach, describe, expect, it } from "vitest";
+import { LogService } from ".";
 
 describe("#LogService", () => {
+  let logService: LogService;
+  beforeEach(() => {
+    logService = new LogService();
+  });
   it("adds new event type", () => {
-    expect(2 + 2).toBe(4);
+    logService.create({
+      type: "MESSAGE",
+      description: "sent message to @foo",
+      timestamp: "9/8/2023, 12:19:15 AM",
+    });
+
+    expect(logService.getLogsByType("MESSAGE")[0].description).toBe(
+      "sent message to @foo"
+    );
   });
 });
