b555237aa50d20b60361352ff803a067b8ae5815
Paul Adams <ghusiyy@gmail.com>
2023-09-10 00:52:43 -0700

    remove unused code

--- project23-instant-messaging/src/lib/services/log/logInstance.ts
+++ project23-instant-messaging/src/lib/services/log/logInstance.ts
@@ -1,4 +1,2 @@
-import { LogCoordinator } from "$lib/models/log";
 import { LogService } from ".";
-const logCoordinator = LogCoordinator.getInstance();
-export const logInstance = new LogService(logCoordinator);
+export const logInstance = new LogService();
