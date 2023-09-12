62273f9934b87e509047ea463b1d12352b09fa4f
Paul Adams <ghusiyy@gmail.com>
2023-09-11 23:29:52 -0700

    init user guide docs

--- project23-instant-messaging/src/routes/guide/+page.svelte
+++ project23-instant-messaging/src/routes/guide/+page.svelte
@@ -1,1 +1,58 @@
-User Guide
+<h1>User Guide: Getting Started with Our App</h1>
+
+<h2>1. User Accesses the App</h2>
+<p>User arrives at the app's landing page.</p>
+
+<h2>2. Single-Use Token Generation</h2>
+<p>
+  The app generates a unique single-use token for the user's current session.
+</p>
+
+<h2>3. Basic Features with Single-Use Token</h2>
+<p>User accesses the following basic features:</p>
+<ul>
+  <li>Profile Creation (Username or Nickname)</li>
+  <li>Profile Customization (Optional)</li>
+  <li>Activity Feed</li>
+  <li>Anonymous Interactions</li>
+  <li>Basic Notifications</li>
+  <li>Limited Messaging (Optional)</li>
+  <li>Bookmarking or Favorites</li>
+  <li>Basic Privacy Settings</li>
+  <li>Content Creation (Limited)</li>
+  <li>Basic Analytics</li>
+  <li>Single-Use Token History</li>
+</ul>
+
+<h2>4. Token Expiry and Re-generation</h2>
+<p>When the user leaves the app:</p>
+<ul>
+  <li>The current single-use token will expire.</li>
+</ul>
+<p>Upon revisiting the app:</p>
+<ul>
+  <li>
+    The user can enter their previous token to pick up where they left off.
+  </li>
+  <li>If needed, a new single-use token can be generated for their session.</li>
+  <li>Token history is maintained for user convenience.</li>
+</ul>
+
+<h2>5. Managing User Data</h2>
+<p>
+  Users are informed about the token system and its advantages in maintaining
+  session continuity.
+</p>
+<p>
+  Optionally, users can provide additional information over time for a more
+  personalized experience.
+</p>
+
+<h2>6. Encouraging Registration</h2>
+<p>If users want a more persistent experience across sessions:</p>
+<ul>
+  <li>They can be encouraged to register for a full account.</li>
+  <li>
+    Registered users can access advanced features and personalized experiences.
+  </li>
+</ul>
